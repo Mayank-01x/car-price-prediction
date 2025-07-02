ff903e671e8bbf7281e543a0f2fe1ace8cf0d063
Mayank-01x <mayank.420.m@gmail.com>
2025-07-02 22:13:33 +0530

    Fix: updated Flask app to use dynamic port for Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
+import os
 import pickle
 
 app = Flask(__name__)
@@ -26,4 +27,6 @@ def predict():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
